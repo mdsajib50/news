11e9bf37025a4923553f86b979c685c925e08ebd
mdsajib50 <mdajib850@gmail.com>
2022-09-04 20:02:05 +0600

    news count done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-try {
+
+  try {
     fetch('https://openapi.programming-hero.com/api/news/categories')
     .then((res) => res.json())
     .then(data => categoriesItem(data.data.news_category
@@ -6,12 +7,14 @@ try {
 } catch (error) {
     alert(error)
 }
+
+
 const categoryList = document.getElementById('category-list');
 
 const categoriesItem =(categories) => {
  
 for (const category of categories) {
-    // console.log(category)
+
     const li = document.createElement('li');
     li.classList.add("nav-item");
     li.innerHTML=`
@@ -25,6 +28,7 @@ for (const category of categories) {
 }
 
 // news 
+let newsCount = document.getElementById('news-count')
 const newsItemId = async (id) =>{
   toggleLoader(true)
     try {
@@ -32,11 +36,12 @@ const newsItemId = async (id) =>{
         const url = `https://openapi.programming-hero.com/api/news/category/${id}`
        const res= await fetch(url)
         const newsData = await res.json()
-        // console.log(newsData)
+        newsCount.innerText= newsData.data.length;
+    
         newsDisplay(newsData.data);
     
     } catch (err) {
-        console.log(err)
+        alert(err)
     }
     
 }
@@ -46,30 +51,29 @@ const newsDisplay= news =>{
     newsItem.textContent='';
        if (news.length !='0') {
       
-           news.forEach(value => {
-              //  console.log(value);
+           news.forEach(key => {
                
                const div = document.createElement('div');
                
                div.classList.add("row", "g-0", "border", "border-info", "rounded", "m-2");
               div.innerHTML =`
               <div class="col-md-4">
-              <img src="${value.thumbnail_url}" class="img-fluid rounded-start mx-auto" alt="...">
+              <img src="${key.thumbnail_url}" class="img-fluid rounded-start mx-auto" alt="...">
             </div>
             <div class="col-md-8">
               <div class="card-body">
-                <h5 class="card-title">${value.title ? value.title: "Title not found"}</h5>
-                <p class="card-text" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">${value.details.slice(0,200)}</p>
+                <h5 class="card-title">${key.title ? key.title: "Title not found"}</h5>
+                <p class="card-text" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">${key.details.slice(0,200)}</p>
                 <div class="d-flex float-start mt-2">
-                <img src="${value.author.img ? value.author.img: "Image not found"}" class="img rounded-5 mx-2" alt="author" style="width:80px; height:80px">
+                <img src="${key.author.img ? key.author.img: "Image not found"}" class="img rounded-5 mx-2" alt="author" style="width:80px; height:80px">
                 <div>
-                <p class="card-text mb-0 mt-3"><small>${value.author.name? value.author.name : 'Name not found' }</small></p>
-                <p class="card-text"><small class="text-muted">${value.author.published_date ? value.author.published_date : 'Date not found' }</small></p>
+                <p class="card-text mb-0 mt-3"><small>${key.author.name? key.author.name : 'Name not found' }</small></p>
+                <p class="card-text"><small class="text-muted">${key.author.published_date ? key.author.published_date : 'Date not found' }</small></p>
                 </div>
                 <div>
-                <p class="card-text text-end mt-4 px-3"><img src="image/icons8-eye-30.png"><small class="text-muted">${value.total_view ? value.total_view : 'Total view not found'}</small></p>
+                <p class="card-text text-end mt-4 px-3"><img src="image/icons8-eye-30.png"><small class="text-muted">${key.total_view ? key.total_view : 'Total view not found'}</small></p>
                 <!-- Button trigger modal -->
-                <button onclick="newsDetailsId('${value._id}')"type="button" class="btn btn-primary mt-5 mx-5" data-bs-toggle="modal" data-bs-target="#exampleModal"> News Details
+                <button onclick="newsDetailsId('${key._id}')"type="button" class="btn btn-primary mt-5 mx-5" data-bs-toggle="modal" data-bs-target="#exampleModal"> News Details
                 </button>
                 
                
@@ -92,32 +96,34 @@ const newsDetailsId = async id =>{
         const url = ` https://openapi.programming-hero.com/api/news/${id}`
        const res= await fetch(url)
         const newsData = await res.json()
-        // const newsData.data = data
+      
         newsData.data.forEach(data=>{
-          console.log(data)
+      
           newsDetails(data)
         })
 
     } catch (err) {
-        console.log(err)
+        alert(err)
     }
     
 }
-// newsDetailsId(id);
+
 const newsModal = document.getElementById('exampleModal')
 
 const newsDetails = news=>{
-        console.log(news);
+       
+      const image= document.getElementById('image');
+      image.innerHTML=`<img src="${news.thumbnail_url}" class="img-fluid rounded-start mx-auto" alt="...">`
         
-        const titleDiv = document.getElementById('exampleModalLabel');
+    const titleDiv = document.getElementById('exampleModalLabel');
         
     titleDiv.innerText= news.title;
     const description=document.getElementById('description');
     description.innerText= news.details;
     const authorName = document.getElementById('author-name');
-    authorName.innerText= news.author.name;
+    authorName.innerText= news.author.name ? news.author.name: 'Name not found';
     const publish =document.getElementById('publish');
-    publish.innerText= news.author.published_date;
+    publish.innerText= news.author.published_date ? news.author.published_date : 'Date not found';
 }
     
 const toggleLoader= isLoading=>{
@@ -127,5 +133,49 @@ const toggleLoader= isLoading=>{
   }else(
     loader.classList.add('d-none')
   )
+}
+
+const newsSort = async () =>{
+  newsItem.textContent='';
+  const url = 'https://openapi.programming-hero.com/api/news/category/08'
+       const res= await fetch(url)
+       const data= await res.json();
+       
+    for (const key of data.data) {
+
+     if (key.total_view > '200' ) {
+       
+      newsCount.innerText='';
+      const div = document.createElement('div');
+               
+               div.classList.add("row", "g-0", "border", "border-info", "rounded", "m-2");
+              div.innerHTML =`
+              <div class="col-md-4">
+              <img src="${key.thumbnail_url}" class="img-fluid rounded-start mx-auto" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">${key.title ? key.title: "Title not found"}</h5>
+                <p class="card-text" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">${key.details.slice(0,200)}</p>
+                <div class="d-flex float-start mt-2">
+                <img src="${key.author.img ? key.author.img: "Image not found"}" class="img rounded-5 mx-2" alt="author" style="width:80px; height:80px">
+                <div>
+                <p class="card-text mb-0 mt-3"><small>${key.author.name? key.author.name : 'Name not found' }</small></p>
+                <p class="card-text"><small class="text-muted">${key.author.published_date ? key.author.published_date : 'Date not found' }</small></p>
+                </div>
+                <div>
+                <p class="card-text text-end mt-4 px-3"><img src="image/icons8-eye-30.png"><small class="text-muted">${key.total_view ? key.total_view : 'Total view not found'}</small></p>
+                <!-- Button trigger modal -->
+                <button onclick="newsDetailsId('${key._id}')"type="button" class="btn btn-primary mt-5 mx-5" data-bs-toggle="modal" data-bs-target="#exampleModal"> News Details
+                </button>
+                
+               
+             </div>
+            </div>
+            </div>`
+           
+            newsItem.appendChild(div);
+     }
+  }
 }
 newsItemId('08');
